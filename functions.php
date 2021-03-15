5509473dee4cc8d1552c4b1c4e3e9bffe0fbf2db
kondo tomonori <tomonorik1001@gmail.com>
2021-03-16 06:01:24 +0900

    :tada:

--- functions.php
+++ functions.php
@@ -2,6 +2,19 @@
 function init_func() {
   add_theme_support('title-tag');
   add_theme_support('post-thumbnails');
+
+  register_post_type('item', [
+    'labels' => [
+      'name' => '商品',
+    ],
+    'public' => true,
+    'has_archive' => true,
+    // ディレクトリの階層をつくれる
+    'hierarchical' => false,
+    // 管理画面上の表示位置
+    'menu_position' => 5,
+    'menu_icon' => 'dashicons-cart'
+  ]);
 }
 add_action('init','init_func');
 
